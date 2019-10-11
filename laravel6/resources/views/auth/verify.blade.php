02f40ab6948a0267ac535a7f0eb241a5e619bcd6
Tedir Ghazali <tedirghazali@gmail.com>
2019-10-11 11:12:41 +0700

    email verification and password reset laravel6

--- laravel6/resources/views/auth/verify.blade.php
+++ laravel6/resources/views/auth/verify.blade.php
@@ -5,12 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Please, Verify Your Email Address') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
+                            <p>{{ __('This is a custom text') }}</p>
                         </div>
                     @endif
 
